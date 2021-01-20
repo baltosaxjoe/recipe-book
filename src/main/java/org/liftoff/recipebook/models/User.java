e506425e53b3d9381d8f900bf0f577af20332706
Joseph Hendricks <jah8w4@mail.missouri.edu>
2021-01-20 17:29:06 -0600

    added follower field to user class

--- src/main/java/org/liftoff/recipebook/models/User.java
+++ src/main/java/org/liftoff/recipebook/models/User.java
@@ -3,7 +3,9 @@ package org.liftoff.recipebook.models;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 public class User extends AbstractEntity{
@@ -13,6 +15,9 @@ public class User extends AbstractEntity{
 
     @NotNull
     private String pwHash;
+
+//    @OneToMany
+//    private List<User> followedUsers;
 
     public User () {}
 
